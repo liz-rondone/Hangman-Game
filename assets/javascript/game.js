1b1b5cb1220ebfea9e791cc7707bfff5e44c3892
liz-rondone <liz.rondone@northwestern.edu>
2017-05-20 17:24:18 -0500

    good enough for now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,19 @@
 // won alert: You are a beautiful tropical fish. You’re smart as a whip and you’re cool under pressure.
 // lost alert: Go back to stupid Eagleton with your stupid face!
 
-
-	// define the words to use
-	var randomWordArr = ["pawnee", "eagleton", "breakfast", "swag", "jammed", "wamapoke", "fartattack", "sweetums"];
-
-	// choose a random word from the array
-	var randomWord = randomWordArr[Math.floor(Math.random() * randomWordArr.length)];
-
 	// global variables
 	var blank;
 	var count = 0;
 	// empty array to store guesses
 	var answerArray = [];
+	var spaceCount = null;
+
+	// define the words to use
+	var randomWordArr = ["pawnee", "eagleton", "breakfast", "swag", "jammed", "wamapoke", "fartattack", "sweetums"];
+	//var fullAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+	// choose a random word from the array
+	var randomWord = randomWordArr[Math.floor(Math.random() * randomWordArr.length)];
+	spaceCount = randomWord.length;
 
 
 	// fill the answer array with underscores
@@ -45,6 +46,11 @@
 
 		// make sure we have a guess (more checks can be made here, only letters etc.)
 		// var letter = document.getElementById("letter").value;
+		/*if (fullAlphabet.indexOf(userText) !-=) {
+			charUsed.push(" " + userText);
+			document.querySelector('#alphaUsed').innerHTML = charUsed;
+		}*/
+
 		for (var i = 0; i < randomWord.length; i++)
 		{
 		  // now if the random word contains a letter that the user typed
@@ -52,6 +58,8 @@
 		  {
 		    // assign it to a letter
 		    answerArray[i] = letter;
+		    spaceCount -=1;
+		    console.log(spaceCount);
 		  }
 		}
 
@@ -72,11 +80,14 @@
 
 	function checkGameEnd()
 	{
-		for (var i = 0; i < randomWord.length; i++) 
+		//for (var i = 0; i < randomWord.length; i++) 
 		{
 
-		    // game win
-		    if (answerArray === randomWord[i]) 
+		   
+		}
+
+		 // game win
+		    if (spaceCount === 0) 
 		    {
 		    	alert("YOU WIN! You are a beautiful tropical fish. You’re smart as a whip and you’re cool under pressure.");
 		    }
@@ -85,7 +96,9 @@
 		    {
 		    	alert("YOU LOSE! Go back to stupid Eagleton with your stupid face.");
 		    }
-		}
+
+		    
+		    //document.getElementById("refresh").innerHTML = location.reload();
 	};
 
 
@@ -94,7 +107,7 @@
 var lives ;
 
 	var showLives = document.getElementById("mylives");
-	
+
    comments = function () 
    {
     if (lives < 1) 
